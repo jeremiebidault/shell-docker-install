5782b575179c26c32c35b40627b35bbcaf3ba1cc
Jérémie Bidault <jeremie.bidault@gmail.com>
2020-09-30 23:46:45 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum update -y
+yum install -y docker-ce docker-ce-cli containerd.io --nobest
+mkdir /etc/docker
+
+cat <<EOF > /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ]
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+systemctl daemon-reload
+systemctl enable docker --now
